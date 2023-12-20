8ff41201846ec29f093c4d141b3a231d878ed1c8
FbeN31 <frolytheking@gmail.com>
2023-12-20 19:41:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/eggapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.eggapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int counter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        counter = 10;
+        TextView counterTextView = findViewById(R.id.textview_count);
+        counterTextView.setText("" + counter);
+    }
+
+    public void knock(View view)
+    {
+        if (counter > 0)
+        {
+            counter--;
+            TextView counterTextView = findViewById(R.id.textview_count);
+            counterTextView.setText("" + counter);
+        }
+        if (counter == 0)
+        {
+            ImageView eggImageView = findViewById(R.id.imageview_egg);
+            eggImageView.setImageResource(R.drawable.surprise_egg);
+        }
+    }
+
+
+    public void reset(View view)
+    {
+        counter = 10;
+        TextView counterTextView = findViewById(R.id.textview_count);
+        counterTextView.setText("" + counter);
+        ImageView eggImageView = findViewById(R.id.imageview_egg);
+        eggImageView.setImageResource(R.drawable.green_egg);
+    }
+}
